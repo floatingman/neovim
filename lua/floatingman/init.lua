b93e0372cec68404ddd6f7d0011490f3568cfd1d
Daniel Newman <dan@danlovesprogramming.com>
2022-08-06 21:15:24 -0500

    Remove all the cruft

---
+++ lua/floatingman/init.lua
@@ -0,0 +1,36 @@
+require("floatingman.set")
+
+local augroup = vim.api.nvim_create_augroup
+FloatingmanGroup = augroup('Floatingman', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = FloatingmanGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = FloatingmanGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
